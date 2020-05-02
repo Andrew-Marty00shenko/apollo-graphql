c4a8033cb3fd7e684524992a4ee05aeb31227c18
Andrew-Marty00shenko <andriei.martyshienko@mail.ru>
2020-05-02 17:06:40 +0300

    Add logout button

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -2,21 +2,50 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import gql from "graphql-tag";
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login'
 import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-    uri: 'http://localhost:4000/'
+    uri: 'http://localhost:4000',
+
 });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache,
-    link
+    link: new HttpLink({
+        uri: 'http://localhost:4000/graphql',
+
+        headers: {
+            authorization: localStorage.getItem('token'),
+        },
+    }),
+    typeDefs,
+    resolvers
 });
+
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem('token'),
+        cartItems: [],
+    },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+    const { data } = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 ReactDOM.render(
@@ -25,6 +54,8 @@ ReactDOM.render(
     </ApolloProvider>,
     document.getElementById('root')
 );
+
+
 
 client
     .query({
--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -10,6 +10,7 @@ import { LAUNCH_TILE_DATA } from './launchTileData';
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
+      isInCart @client
       site
       rocket {
         type
